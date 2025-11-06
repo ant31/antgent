e99e33af517be235fbe5418ac6e4748b9a8b6215
ant31 <2t.antoine@gmail.com>
2025-11-06 14:43:30 +0100

    Bump

--- antgent/__init__.py
+++ antgent/__init__.py
@@ -7,7 +7,7 @@ from .models.agent import AgentConfig, AgentInput, AgentWorkflowOutput
 from .models.message import Content
 from .workflows.base import BaseWorkflow, WorkflowInput
 
-__version__ = "0.12.0"
+__version__ = "0.12.1"
 
 
 __all__ = [
